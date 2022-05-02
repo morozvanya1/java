30455c6b81b2714f0d3f78b6e01cd154827ddf79
Ivan M <im@kalite.ro>
2022-05-02 12:37:57 +0300

    Warmup-1 > sleepIn

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import Warmup.SleepIn;
+
 public class Main {
 
     public static void main(String[] args) {
         //Hello world
         System.out.println("Hello world");
+
+        SleepIn s = new SleepIn();
+
+        System.out.println(s.sleepIn(false, false));
+        System.out.println(s.sleepIn(true, false));
+        System.out.println(s.sleepIn(false, true));
     }
 }
