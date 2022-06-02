40fbe1a3cf1156cd47e08b69d93d6b3fdd9abfb0
Ivan M <im@kalite.ro>
2022-06-02 23:23:53 +0300

    Warmup-1 > in3050

--- src/CodingBat/Warmup.java
+++ src/CodingBat/Warmup.java
@@ -155,4 +155,8 @@ public class Warmup {
         return newA == newB ? 0 : newA > newB ? b : a;
     }
 
+    public boolean in3050(int a, int b) {
+        return (a >= 30 && a <= 40) && (b >= 30 && b <= 40) || (a >= 40 && a <= 50) && (b >= 40 && b <= 50);
+    }
+
 }
