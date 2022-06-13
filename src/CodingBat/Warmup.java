b20e0c5bae255fa85e49a5777b8a652a8791f2b3
Ivan M <im@kalite.ro>
2022-06-13 22:04:08 +0300

    Warmup-1 > endUp

--- src/CodingBat/Warmup.java
+++ src/CodingBat/Warmup.java
@@ -184,4 +184,11 @@ public class Warmup {
         return a % 10 == b % 10;
     }
 
+    public String endUp(String str) {
+        if (str.length() > 3) {
+            return str.substring(0, str.length()-3) + str.substring(str.length()-3).toUpperCase();
+        }
+        return str.toUpperCase();
+    }
+
 }
