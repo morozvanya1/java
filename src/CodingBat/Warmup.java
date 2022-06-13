5614a3aba0aa0f56b266e5db5f89ebd29414d487
Ivan M <im@kalite.ro>
2022-06-13 21:59:07 +0300

    Warmup-1 > stringE

--- src/CodingBat/Warmup.java
+++ src/CodingBat/Warmup.java
@@ -170,4 +170,14 @@ public class Warmup {
         return 0;
     }
 
+    public boolean stringE(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'e') {
+                count++;
+            }
+        }
+        return count == 1 || count == 3;
+    }
+
 }
