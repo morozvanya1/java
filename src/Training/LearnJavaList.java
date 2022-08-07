2bdeec8074705f19819cf11cef379074c9848ea2
Ivan M <im@kalite.ro>
2022-08-07 14:37:23 +0300

    Learn Java

--- src/Training/LearnJavaList.java
+++ src/Training/LearnJavaList.java
@@ -1,8 +1,10 @@
 package Training;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Random;
+
 
 public class LearnJavaList {
 
@@ -11,7 +13,7 @@ public class LearnJavaList {
         ArrayList<Integer> myList = new ArrayList<>();
         HashMap<Integer, Integer> myHash = new HashMap<>();
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 5; i > 0; i--) {
             myList.add(i);
             myHash.put(i, new Random().nextInt());
         }
@@ -19,11 +21,13 @@ public class LearnJavaList {
         for (int list : myList) {
             System.out.println(list);
         }
+        System.out.println(myList);
 
         for (int i = 0; i < myHash.size(); i++) {
             System.out.println(myHash.get(i));
         }
 
-
+        Collections.sort(myList);
+        System.out.println(myList);
     }
 }
