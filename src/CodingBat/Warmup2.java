35014a49503c7e5135230f765f7e3ff803cb66ac
Ivan M <im@kalite.ro>
2022-08-21 02:53:30 +0300

    Warmup-2 > array123

--- src/CodingBat/Warmup2.java
+++ src/CodingBat/Warmup2.java
@@ -91,4 +91,13 @@ public class Warmup2 {
         return false;
     }
 
+    public boolean array123(int[] nums) {
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] == 1 && nums[i+1] == 2 && nums[i+2] == 3) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
