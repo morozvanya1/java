5a5d6c5e30c1d907d8f11cd1a785f20967cde03a
Ivan M <im@kalite.ro>
2022-11-15 15:50:29 +0300

    Array-2 (part 1)

--- src/CodingBat/pages/Array2.java
+++ src/CodingBat/pages/Array2.java
@@ -220,6 +220,17 @@ public class Array2 {
     }
 
     public boolean modThree(int[] nums) {
+        for (int i = 1; i < nums.length-1; i++) {
+            if (nums[i-1] % 2 == 0 && nums[i] % 2 == 0 && nums[i+1] % 2 == 0) {
+                return true;
+            } else if (nums[i-1] % 2 != 0 && nums[i] % 2 != 0 && nums[i+1] % 2 != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean haveThree(int[] nums) {
 
     }
 
