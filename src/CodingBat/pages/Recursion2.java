0e4861caa4667aaf05aeb8a644363f397aa00369
Ivan M <im@kalite.ro>
2023-01-11 13:33:08 +0300

    Recursion-2

--- src/CodingBat/pages/Recursion2.java
+++ src/CodingBat/pages/Recursion2.java
@@ -27,7 +27,20 @@ public class Recursion2 {
     }
 
     public boolean groupSum5(int start, int[] nums, int target) {
+        if (start >= nums.length) return (target == 0);
+        if (nums[start] % 5 == 0) {
+            if (start + 1 < nums.length && nums[start + 1] == 1) {
+                nums[start + 1] = 0;
+            }
+            target -= nums[start];
+        }
+        if (groupSum5(start + 1, nums, target - nums[start])) return true;
+        if (groupSum5(start + 1, nums, target)) return true;
         return false;
+    }
+
+    public boolean groupSumClump(int start, int[] nums, int target) {
+
     }
 
 }
